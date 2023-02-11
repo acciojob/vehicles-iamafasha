836eccb64cd82ba169c0e47c3ec9514a93e99a49
Afasha Isakiye <afashaisakiye@gmail.com>
2023-02-11 13:40:41 +0100

    fix

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -14,7 +14,7 @@ public class Vehicle {
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
-        currentDirection = direction;
+        currentDirection + = direction;
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
@@ -39,7 +39,7 @@ public class Vehicle {
     }
     
     public void stop(){
-        currentSpeed = 0;
+        this.currentSpeed = 0;
         System.out.println("stop method called - The vehicle is stopped");
     }
 
