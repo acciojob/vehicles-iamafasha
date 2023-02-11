a4fec68a0c85710dc5404857a30f40b23a074cbd
Afasha Isakiye <afashaisakiye@gmail.com>
2023-02-11 13:28:19 +0100

    FIX

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,12 @@ public class Boat implements WaterVehicle {
 
     private String name;
     private int capacity;
+
+    public void Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    } 
+
     public String getVehicleName(){
         return this.name;
     }
