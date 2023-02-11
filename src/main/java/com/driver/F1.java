f5e4641e857ce9a49c4c0e1f1c7129012c6e9c3a
Afasha Isakiye <afashaisakiye@gmail.com>
2023-02-11 13:57:19 +0100

    fix

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -25,13 +25,13 @@ public class F1 extends Car {
             this.changeGear(1);
         }else if(newSpeed > 0 && newSpeed <= 50 ){
             this.changeGear(1);
-        }else if(newSpeed > 51 && newSpeed <= 100 ){
+        }else if(newSpeed > 50 && newSpeed <= 100 ){
             this.changeGear(2);
-        }else if(newSpeed > 101 && newSpeed <= 150 ){
+        }else if(newSpeed > 100 && newSpeed <= 150 ){
             this.changeGear(3);
-        }else if(newSpeed > 151 && newSpeed <= 200 ){
+        }else if(newSpeed > 150 && newSpeed <= 200 ){
             this.changeGear(4);
-        }else if(newSpeed > 201 && newSpeed <= 250 ){
+        }else if(newSpeed > 200 && newSpeed <= 250 ){
             this.changeGear(5);
         }else {
             this.changeGear(6);
