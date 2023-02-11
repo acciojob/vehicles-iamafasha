5be2381911dcf7a9b14547bf63324b2726c9927c
Afasha Isakiye <afashaisakiye@gmail.com>
2023-02-11 13:32:01 +0100

    remove void

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -5,7 +5,7 @@ public class Boat implements WaterVehicle {
     private String name;
     private int capacity;
 
-    public void Boat(String name, int capacity){
+    public Boat(String name, int capacity){
         super();
         this.name = name;
         this.capacity = capacity;
